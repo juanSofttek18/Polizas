2c153ed6457d697ac556d4e44464b2798771fb08
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-04-07 16:49:50 +0200

    main

--- example/Main.java
+++ example/Main.java
@@ -1,4 +1,15 @@
 package helloworld.liquidaciondiversos.example;
 
+import helloworld.liquidaciondiversos.modulo.Cliente;
+import helloworld.liquidaciondiversos.modulo.Poliza;
+
+import javax.xml.crypto.Data;
+
 public class Main {
+    public static void main(String[] args) {
+        BaseData baseData=new BaseData();
+        Cliente cliente=new Cliente("Juan Perez","12345678","Calle Falsa 123");
+        Poliza poliza=new Poliza(baseData.getProduct(),cliente,10000);
+    }
+
 }
