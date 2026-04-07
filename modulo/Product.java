9616f112c68929dbf7c305281908c5099658cc2b
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-04-07 12:53:47 +0200

    quitado el import

--- modulo/Product.java
+++ modulo/Product.java
@@ -1,14 +1,33 @@
 package helloworld.liquidaciondiversos.modulo;
 
-
-import lombok.Data;
-
 import java.util.List;
 
-@Data
 public class Product {
     private String code;
     private String name;
     List<ProductWarranty> productWarranties;
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ProductWarranty> getProductWarranties() {
+        return productWarranties;
+    }
+
+    public void setProductWarranties(List<ProductWarranty> productWarranties) {
+        this.productWarranties = productWarranties;
+    }
 }
