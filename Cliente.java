45e6e9b37ae00516dfde7e94776e252318d2bc46
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-04-07 12:20:04 +0200

    muevo a modulo las clases y ya puse los datos del cliente

--- Cliente.java
+++
@@ -1,4 +0,0 @@
-package helloworld.liquidaciondiversos;
-
-public class Cliente {
-}
---
+++ modulo/Cliente.java
@@ -0,0 +1,8 @@
+package helloworld.liquidaciondiversos.modulo;
+
+public class Cliente {
+    
+    private String nombre;
+    private String dni;
+    private String direccion;
+}
